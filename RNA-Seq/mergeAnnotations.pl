8483cb40467b8fa5ef62394758a7637462337822
parikhas <parikh.as@husky.neu.edu>
2017-10-18 19:15:13 -0400

    Create FASTA Sampler

--- RNA-Seq/mergeAnnotations.pl
+++ RNA-Seq/mergeAnnotations.pl
@@ -1,11 +1,35 @@
 #!/bin/perl
 use Bio::SearchIO;
+use Bio::Search::Result::GenericResult;
+use Data::Dumper;
+use Bio::SeqIO;
+
+open( $tsvfile, ">", "aipSwissProt.tsv" ) or die $!;
+
+print $tsvfile("Trinity","\t","SwissProt","\t","SwissProtDesc","\t","eValue","\n");
 
 my $blastXml = Bio::SearchIO->new(
-	-file => 'Trinity-GG.blastp.xml',
+	-file   => 'Trinity-GG.blastp.xml',
 	-format => 'blastxml'
 );
 
-while (my $result = $blastXml->next_result()){
-	print $result, "\n";
+while ( my $result = $blastXml->next_result() ) {
+	my $queryDesc = $result->query_description;
+	if ( $queryDesc =~ /::(.*?)::/ ) {
+		my $queryDescShort = $1;
+		my $hit            = $result->next_hit;
+		if ($hit) {
+			print $tsvfile ( $queryDescShort,"\t");
+			print $tsvfile ( $hit->accession,"\t");
+			my $subjectDescription = $hit->description;
+			if ( $subjectDescription =~ /Full=(.*?);/ ) {
+				$subjectDescription = $1;
+			}
+			if ( $subjectDescription =~ /Full=(.*?)\[/ ) {
+				$subjectDescription = $1;
+			}
+			print $tsvfile ($subjectDescription,"\t");
+			print $tsvfile ($hit->significance,"\n");
+		}
+	}
 }
