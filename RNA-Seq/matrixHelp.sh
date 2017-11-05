34533e71336d28c814d69250a2cae01011e2cc13
parikhas <parikh.as@husky.neu.edu>
2017-11-05 14:34:45 -0500

    Updating github

--- RNA-Seq/matrixHelp.sh
+++ RNA-Seq/matrixHelp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#see the options for the util abundance_estimates_to_matrix.pl
 trinityPath='/usr/local/programs/trinityrnaseq-2.2.0'
 nice -n19 $trinityPath/util/abundance_estimates_to_matrix.pl
