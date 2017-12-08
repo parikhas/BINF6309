f22c1e6cfa1486d6a10aa7895b2ec5e27183b018
parikhas <parikh.as@husky.neu.edu>
2017-12-07 23:41:29 -0500

    edits

--- VariantCalling/recalibrateBases2.sh
+++ VariantCalling/recalibrateBases2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-baseCommand='nice -n19 java jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar'
+baseCommand='nice -n19 java -jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar'
 
 taskRef='-T PrintReads -nct 16 -R vShiloni.fasta -I merged.bam -BQSR recal_data.table -o recal_reads.bam'
 
